1c7acd59b2a3ace1b901c356b119954a9fabb307
spichy <Sufursky@gmail.com>
2013-09-22 13:07:00 +0200

    Update 2013_08_27_02_world_reputation_reward_rate.sql
    Typo

--- sql/updates/world/2013_08_27_02_world_reputation_reward_rate.sql
+++ sql/updates/world/2013_08_27_02_world_reputation_reward_rate.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `reputation_reward_rate` ADD `quest_repeteable_rate` FLOAT NOT NULL DEFAULT '1' AFTER `quest_weekly_rate`;
+ALTER TABLE `reputation_reward_rate` ADD `quest_repeatable_rate` FLOAT NOT NULL DEFAULT '1' AFTER `quest_weekly_rate`;
